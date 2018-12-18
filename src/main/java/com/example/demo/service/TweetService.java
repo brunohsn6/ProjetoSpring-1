ccf5191c12b7f2e47239b45a6406354cd305b369
Mariana <quirino193@gmail.com>
2018-12-18 20:40:50 -0300

    Versao 1.0

--- src/main/java/com/example/demo/service/TweetService.java
+++ src/main/java/com/example/demo/service/TweetService.java
@@ -5,7 +5,6 @@ import com.example.demo.model.User;
 import com.example.demo.repository.TweetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
